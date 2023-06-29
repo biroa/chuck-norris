28773573d7aa8f1f775ad8041e5b500aacd208cc
Adam Biro <adamb@portwest.com>
2023-06-29 19:40:48 +0100

    * Install the necessary packages * Init the Route * Return an Inertia Response

---
+++ resources/js/Pages/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Main = () => {
+    const react = "React";
+    const inertia = "Inertia";
+    const laravel = "Laravel";
+
+    return (
+        <h1>
+            {react} + {inertia} + {laravel}
+        </h1>
+    );
+};
+
+export default Main;
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+import { InertiaApp } from '@inertiajs/inertia-react'
+import React from 'react'
+import { createRoot } from 'react-dom/client';
+
+const app = document.getElementById('app')
+const root = createRoot(app);
+
+root.render(
+    <InertiaApp
+        initialPage={JSON.parse(app.dataset.page)}
+        resolveComponent={name => import(`./Pages/${name}`).then(module => module.default)}
+    />
+)
